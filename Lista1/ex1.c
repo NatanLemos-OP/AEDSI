ae05813a894fa51f8a0bd9a1f5a34df141b56373
NatanLemos-OP <0116394@academico.ifmg.edu.br>
2025-09-25 10:37:35 -0300

    Mudanças na função

---
+++ Lista1/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    int soma () {
+        int a=10;
+        int b=20;
+        int c = a+b;
+        printf("%d", c);
+    }
+    int main () {
+        printf("Natan Lemos\n");
+        int c = soma();
+    }
